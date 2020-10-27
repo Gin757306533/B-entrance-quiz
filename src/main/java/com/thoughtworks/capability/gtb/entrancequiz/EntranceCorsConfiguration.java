bf6374e1e135ad9123469b4c14a673d6ddb2e07a
JieXu <jie.xu@thoughtworks.com>
2020-10-27 21:06:15 +0800

    feat[xj-1]: add configuration for cross origins requests

---
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/EntranceCorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.thoughtworks.capability.gtb.entrancequiz;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class EntranceCorsConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+}
